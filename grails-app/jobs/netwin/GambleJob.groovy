57c45a3862043f7573eebb927016cd94351716b0
Clairehuei <clairehuei@gmail.com>
2015-07-20 01:37:12 +0800

    排程設定

--- grails-app/jobs/netwin/GambleJob.groovy
+++ grails-app/jobs/netwin/GambleJob.groovy
@@ -2,12 +2,26 @@ package netwin
 
 
 class GambleJob {
+
+    def NetReptileService
+
     static triggers = {
-      simple repeatInterval: 5000l // execute job once in 5 seconds
+      simple repeatInterval: 10000l // execute job once in 10 seconds
     }
 
     def execute() {
         println "排程開始"
+        def url = "http://www.taiwanlottery.com.tw/index_new.aspx"
+        NetReptileService.getBingo(url)//賓果
+        NetReptileService.getWayLiTry(url)//威力彩
+        NetReptileService.getThreeEightTry(url)//38樂合彩
+        NetReptileService.getBigLuckToa(url)//大樂透
+        NetReptileService.getFourNineTry(url)//49樂合彩
+        NetReptileService.getBigFuTry(url)//大福彩
+        NetReptileService.getFiveThreeNine(url)//今彩539
+        NetReptileService.getThreeNineTry(url)//39樂合彩
+        NetReptileService.getThreeStarTry(url)//3星彩
+        NetReptileService.getFourStarTry(url)//4星彩
         // execute job
     }
 }
