39686fd7d93072c98d895841ab985d0d82d0dcea
Allen <clairehuei1216@gmail.com>
2015-07-24 00:30:12 +0800

    test

--- grails-app/jobs/netwin/GambleJob.groovy
+++ grails-app/jobs/netwin/GambleJob.groovy
@@ -10,18 +10,20 @@ class GambleJob {
     }
 
     def execute() {
-        println "排程開始"
+        def no = System.currentTimeMillis()
+        println "排程["+no+"]開始"+new Date()
         def url = "http://www.taiwanlottery.com.tw/index_new.aspx"
         netReptileService.getBingo(url)//賓果
-        netReptileService.getWayLiTry(url)//威力彩
-        netReptileService.getThreeEightTry(url)//38樂合彩
-        netReptileService.getBigLuckToa(url)//大樂透
-        netReptileService.getFourNineTry(url)//49樂合彩
-        netReptileService.getBigFuTry(url)//大福彩
-        netReptileService.getFiveThreeNine(url)//今彩539
-        netReptileService.getThreeNineTry(url)//39樂合彩
-        netReptileService.getThreeStarTry(url)//3星彩
-        netReptileService.getFourStarTry(url)//4星彩
+//        netReptileService.getWayLiTry(url)//威力彩
+//        netReptileService.getThreeEightTry(url)//38樂合彩
+//        netReptileService.getBigLuckToa(url)//大樂透
+//        netReptileService.getFourNineTry(url)//49樂合彩
+//        netReptileService.getBigFuTry(url)//大福彩
+//        netReptileService.getFiveThreeNine(url)//今彩539
+//        netReptileService.getThreeNineTry(url)//39樂合彩
+//        netReptileService.getThreeStarTry(url)//3星彩
+//        netReptileService.getFourStarTry(url)//4星彩
         // execute job
+        println "排程["+no+"]結束"+new Date()
     }
 }
