0af0885fd00c1b56bb7de58667d7a83d265e2f9c
Jimmy <jimmychou@wezoomtek.com.tw>
2015-08-27 02:44:11 +0800

    1. 新增連續分布分析

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -65,7 +65,7 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
-        compile ":asset-pipeline:2.1.5"
+        compile ":asset-pipeline:2.5.1"
         compile ":quartz:1.0.2"
         compile ":remote-pagination:0.4.8"
         compile ":disqus:0.3"
